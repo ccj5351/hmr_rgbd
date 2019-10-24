9bb7cde49e195e90f5b37dfb33237d9ad8b8e38e
Changjiang_Cai <caicj5351@gmail.com>
2019-10-24 06:05:09 -0400

    ResnetV2 done

--- pytorch_src/ResnetV2.py
+++ pytorch_src/ResnetV2.py
@@ -44,13 +44,13 @@ The key difference of the full preactivation 'v2' variant compared to the
 ########################################
 # Kaiming's blocks
 ########################################
-def conv3x3(cin, cout, stride=1, groups=1):
-    return nn.Conv2d(cin, cout, kernel_size=3, stride=stride, padding=1, bias=False, 
+def conv3x3(cin, cout, stride=1, groups=1, bias=False):
+    return nn.Conv2d(cin, cout, kernel_size=3, stride=stride, padding=1, bias=bias, 
                      groups=groups)
 
 
-def conv1x1(cin, cout, stride=1):
-    return nn.Conv2d(cin, cout, kernel_size=1, stride=stride, padding=0, bias=False)
+def conv1x1(cin, cout, stride=1,bias=False):
+    return nn.Conv2d(cin, cout, kernel_size=1, stride=stride, padding=0, bias=bias)
 
 # bottleneck_v2
 # x-->BN --> ReLU-->(conv1, BN, ReLU)-->(conv2, BN, ReLU) --> conv3
@@ -59,128 +59,73 @@ def conv1x1(cin, cout, stride=1):
 # |                                                             |
 # |--------------------------------------------> Addition --> x_new
 class Bottleneck_V2(nn.Module):
-    def __init__(self, cin, cout=None, cmid=None, stride=1, downsample=None):
+    expansion = 4
+    def __init__(self, cin, cout, stride):
         super(Bottleneck_V2, self).__init__()
-        cout = cout or cin
-        cmid = cmid or cout//4
+        cmid = cout// self.expansion
    
         self.relu = nn.ReLU(inplace=True)
         """ Pre Act """
-        self.preact = nn.BatchNorm2d(cin)
+        self.bn0 = nn.BatchNorm2d(cin)
         
         """ (conv1, BN, ReLU)"""
-        self.conv1 = conv1x1(cin, cmid) #conv1
+        self.conv1 = conv1x1(cin, cmid, bias=False) #conv1
         self.bn1 = nn.BatchNorm2d(cmid) #conv1/BatchNorm
         
         """ (conv2, BN, ReLU)"""
-        self.conv2 = conv3x3(cmid, cmid, stride) #conv2
+        self.conv2 = conv3x3(cmid, cmid, stride, bias=False) #conv2
         self.bn2 = nn.BatchNorm2d(cmid) #conv2/BatchNorm
-        self.conv3 = conv1x1(cmid, cout)
+        """ (conv3 )"""
+        self.conv3 = conv1x1(cmid, cout, bias=True) # conv3
 
         self.stride = stride
-        self.downsample = downsample
-        if (stride != 1 or cin != cout) and downsample in (True, None):
-            self.downsample = conv1x1(cin, cout, stride)
+        self.maxpool2d= nn.MaxPool2d(kernel_size=1, stride = stride)
+        self.shortcut = None
+        if cin != cout:
+            # conv, 1 x 1
+            self.shortcut = conv1x1(cin, cout, stride, bias = True)
 
     def forward(self, x):
-        # Conv'ed branch
-        out = x
-        out = self.conv1(self.relu(self.bn1(out)))
-        out = self.conv2(self.relu(self.bn2(out)))
-        out = self.conv3(self.relu(self.bn3(out)))
-
-        # Residual branch
-        residual = x
-        if self.downsample is not None:
-            residual = self.downsample(residual)
-
-        return out + residual
-
-    def reset_parameters(self):
-        # Following https://github.com/KaimingHe/resnet-1k-layers/blob/master/resnet-pre-act.lua#L129
-        nn.init.kaiming_normal(self.conv1.weight, a=0, mode='fan_out')
-        nn.init.kaiming_normal(self.conv2.weight, a=0, mode='fan_out')
-        nn.init.kaiming_normal(self.conv3.weight, a=0, mode='fan_out')
-
-        # Not the default =(
-        nn.init.constant(self.bn1.weight, 1)
-        nn.init.constant(self.bn2.weight, 1)
-        nn.init.constant(self.bn3.weight, 1)
-        return self
-
-
-# bottleneck_v2
-# x-->BN --> ReLU-->(conv1, BN, ReLU)-->(conv2, BN, ReLU)-->(conv3,BN,ReLU)
-# |                                                                |
-# |                                                                |
-# |                                                                |
-# |-----------------------------------------------> Addition --> x_new
-class Bottleneck_V2(nn.Module):
-    expansion = 4
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(Bottleneck_V2, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
+        """ Pre Act """
+        preact = self.relu(self.bn0(x))
+        if self.shortcut is not None:
+            shortcut = self.shortcut(preact) # e.g., stride = 2
+        else:
+            shortcut = self.maxpool2d(x)
         
-        width = int(planes * (base_width / 64.)) * groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
+        """ (conv1, BN, ReLU)"""
+        residual = self.relu(self.bn1(self.conv1(preact)))
+        """ (conv2, BN, ReLU)"""
+        residual = self.relu(self.bn2(self.conv2(residual)))
+        """ (conv3 )"""
+        residual = self.conv3(residual)
+        output = shortcut + residual
+        return output
 
 
 class ResNet_V2(nn.Module):
-    def __init__(self, block, layers, num_classes=None):
+    def __init__(self, block, layers, num_classes=None, global_pool = True):
         self.inplanes = 64
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
+        super(ResNet_V2, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=True)
         # We do not include batch normalization or activation functions in
         # conv1 because the first ResNet unit will perform these. Cf.
         # Appendix of [2].
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
+        #self.bn1 = nn.BatchNorm2d(64)
+        #self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
         self.layer1 = self._make_layer(block,  64, layers[0], stride=2)
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1)) # output is of size 1 x 1 here;
-        
+        #self.avgpool = nn.AdaptiveAvgPool2d((1, 1)) # output is of size 1 x 1 here;
+        self.global_pool = global_pool
         #Note: in HMR project, we set `num_classes=None`;
         if num_classes is not None:
             self.fc = nn.Linear(512 * block.expansion, num_classes)
+        else:
+            self.fc = None
         
         #leave it here FYI:
         #for m in self.modules():
@@ -199,47 +144,41 @@ class ResNet_V2(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
+    #def __init__(self, cin, cout, stride=1):
 
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
+    def _make_layer(self, block, planes, numBlocks, stride):
+        expansion = block.expansion
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
+        for i in range(0, numBlocks):
+            cur_inplanes = planes * expansion if i > 0 else self.inplanes
+            tmp_stride = 1 if i < (numBlocks - 1) else stride
+            layers.append(block(cur_inplanes, planes*expansion, tmp_stride))
+        
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-       # x = self.fc(x)
-
+        if self.global_pool:
+            x = torch.mean(x, dim=[2,3], keepdim = True)
+        if self.fc is not None:
+            x = self.fc(torch.flatten(x,1))
         return x
 
 
+def resnet_v2_50(num_classes=None, global_pool = True):
+    model = ResNet_V2(Bottleneck_V2, [3,4,6,3],num_classes, global_pool)
+    return model
 
 
 if __name__ == '__main__':
-    encoder = load_Res50Model()
-    print ('resnet-50 state_dict():')
+    encoder = resnet_v2_50()
+    print ('resnet_v2_50 state_dict():')
     n = 0
     for k,v in encoder.state_dict().items():
         print (k, v.shape)
